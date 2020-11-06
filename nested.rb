f49540118089bd9e43ea01ec1cac0b6007d170f5
Duncan Stark <letmunplaysgames@gmail.com>
2020-11-06 22:42:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,5 +1,6 @@
 
 def hopper
+
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -15,12 +16,10 @@ def hopper
         :languages => ["C"]
       }
     }
-
+programmer_hash.fetch(:grace_hopper)
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -36,6 +35,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+  programmer_hash.dig(:alan_kay, :known_for )
 
 end
 
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
