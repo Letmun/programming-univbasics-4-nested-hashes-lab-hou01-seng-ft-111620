38281da96ed48910465e2a40242b3a9b14ae7025
Duncan Stark <letmunplaysgames@gmail.com>
2020-11-06 22:47:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,10 +80,6 @@ def adding_matz
         :known_for => "Unix",
         :languages => ["C"]
       }
-     :yukihiro_matsumoto => {
-  :known_for => "Ruby",
-  :languages => ["LISP", "C"]
- }
     }
 puts programmer_hash
 end
